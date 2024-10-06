1933bbcea5b6bae57ca4d96c84a2735bacd960f5
Saipriya-1144 <saipriyach1144@gmail.com>
2024-10-06 21:37:28 +0530

    modified node sample app

--- __tests__/nodesampleapp/server.js
+++ __tests__/nodesampleapp/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log('Hello endpoint called!');
-    res.send('Hello Hii from NodeJs App, Heyyy holaaa!')
+    res.send('Hello from NodeJs App, Heyyy holaaa!')
 })
 
 app.get('/dummy', (req, res) => {
