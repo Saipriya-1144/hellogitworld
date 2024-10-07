907316adbfb2a01615b9666cd25084508e8f13ea
Saipriya-1144 <saipriyach1144@gmail.com>
2024-10-07 12:47:46 +0530

    added a script file to prebuild the app

--- __tests__/modify-and-build.sh
+++ __tests__/modify-and-build.sh
@@ -9,9 +9,11 @@ cp -r __tests__/nodesampleapp nodeapp
 # Navigate to the nodeapp directory
 cd nodeapp
 
+ls
+
 # Modify the app (example: replace a placeholder in a config file)
 current_utc_time=$(date -u +"%Y-%m-%d %H:%M:%S %Z")
-sed -i "s/<<<node-place-holder>>>/$current_utc_time/g" nodeapp/server.js
+sed -i "s/<<<node-place-holder>>>/$current_utc_time/g" server.js
 
 # Build the app (example: using npm for a Node.js app)
 npm install
