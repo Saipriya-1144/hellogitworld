061d60c3f9b97994e3519674a2a39d4562bfae06
Saipriya-1144 <saipriyach1144@gmail.com>
2024-10-07 14:18:45 +0530

    added a script file to prebuild the dotnet app

--- __tests__/modify-and-build.sh
+++ __tests__/modify-and-build.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Copy the __tests__/nodesampleapp to nodeapp
-cp -r __tests__/nodesampleapp nodeapp
+case $1 in
 
-# Navigate to the nodeapp directory
-cd nodeapp
+ "dotnet")
+    cp -r __tests__/dotnetsampleapp dotnetapp
+    cd dotnetapp
 
-ls
+    current_utc_time=$(date -u +"%Y-%m-%d %H:%M:%S %Z")
+    sed -i "s/<<<net-place-holder>>>/$current_utc_time/g" Controllers/HelloController.cs
 
-# Modify the app (example: replace a placeholder in a config file)
-current_utc_time=$(date -u +"%Y-%m-%d %H:%M:%S %Z")
-sed -i "s/<<<node-place-holder>>>/$current_utc_time/g" server.js
+    # Build the app
+    dotnet build --configuration Release
 
-# Build the app (example: using npm for a Node.js app)
-npm install
-npm run build --if-present
+    echo "Modification and build completed successfully."
+ ;;
 
-echo "Modification and build completed successfully."
+ "nodejs")
+    cp -r __tests__/nodesampleapp nodeapp
+    cd nodeapp
+
+    current_utc_time=$(date -u +"%Y-%m-%d %H:%M:%S %Z")
+    sed -i "s/<<<node-place-holder>>>/$current_utc_time/g" server.js
+
+    # Build the app
+    npm install
+    npm run build --if-present
+
+    echo "Modification and build completed successfully."
+ ;;
+
+esac
+
